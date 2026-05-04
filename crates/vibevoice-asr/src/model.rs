8207af59217f21f11dfc5da9294532fbf1fcf317
Andrew Yourtchenko <ayourtch@gmail.com>
2026-05-04 18:14:27 +0200

    Panic if a runaway segment is detected
    Change-Id: I36e0012cf21e21eb94a3eb9b3c9cae757d6ce123

--- crates/vibevoice-asr/src/model.rs
+++ crates/vibevoice-asr/src/model.rs
@@ -259,6 +259,9 @@ impl VibeVoiceAsrModel {
                         }
                     }
                     accum.push(c);
+                    if accum.len() - acc_start > 4096 {
+                        panic!("RUNAWAY segment detected ({} bytes unparsed), truncating. Unparsed: {:?}", accum.len() - acc_start, &accum[acc_start..]);
+                    }
                     if c != ',' {
                         if let Ok(ts) = serde_json::from_str::<TranscriptionSegment>(&accum[acc_start..]) {
                             eprintln!("\nPARSED: {:?}\n", ts);
